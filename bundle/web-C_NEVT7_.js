19929e644ef68d1efd464c167ebb7ff6be2362de
TiGA Auto Release <actions@github.com>
2026-09-19 08:32:59 +0000

    Auto-release v13.7.348 (OTA)

--- bundle/web-C_NEVT7_.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:bundle/web-C_NEVT7_.js
-import{W as o}from"./index.template-CQru-RhE.js";class e extends o{constructor(){super(),this._lastWindow=null}async open(s){this._lastWindow=window.open(s.url,s.windowName||"_blank")}async close(){return new Promise((s,n)=>{this._lastWindow!=null?(this._lastWindow.close(),this._lastWindow=null,s()):n("No active window to close!")})}}const i=new e;export{i as Browser,e as BrowserWeb};
-========
-import{W as o}from"./index.template-BVPWRTVN.js";class e extends o{constructor(){super(),this._lastWindow=null}async open(s){this._lastWindow=window.open(s.url,s.windowName||"_blank")}async close(){return new Promise((s,n)=>{this._lastWindow!=null?(this._lastWindow.close(),this._lastWindow=null,s()):n("No active window to close!")})}}const i=new e;export{i as Browser,e as BrowserWeb};
->>>>>>>> 0934166a66be118d577c82bb6f404676f3035950:bundle/web-DMQQBOfz.js
