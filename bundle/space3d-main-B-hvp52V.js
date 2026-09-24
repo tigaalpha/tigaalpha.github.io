5ed3ae3918470bc64f2d885363ffa67da12e589f
TiGA Auto Release <actions@github.com>
2026-09-24 23:04:27 +0000

    Auto-release v13.7.398 (OTA)

--- bundle/space3d-main-B-hvp52V.js
+++ bundle/space3d-main-B-hvp52V.js
@@ -1,4 +1,4 @@
-import{r as Z,an as cS,j as oe,ao as ep}from"./index.template-WV3apFuf.js";/**
+import{r as Z,an as cS,j as oe,ao as ep}from"./index.template-BhP_DgV3.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
