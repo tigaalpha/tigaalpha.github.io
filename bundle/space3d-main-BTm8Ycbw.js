bccd12fa8ac61f56032ce9f365a57ac9d57d9e6c
TiGA Auto Release <actions@github.com>
2026-09-25 08:42:39 +0000

    Auto-release v13.7.439 (OTA)

--- bundle/space3d-main-BTm8Ycbw.js
+++ bundle/space3d-main-BTm8Ycbw.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-BLaZP7Cj.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-1gRNMehm.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
