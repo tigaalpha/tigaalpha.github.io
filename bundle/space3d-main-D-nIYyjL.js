b087de8f4acacb247b43ba45b6295f0ff0721f84
TiGA Auto Release <actions@github.com>
2026-09-25 03:58:47 +0000

    Auto-release v13.7.418 (OTA)

--- bundle/space3d-main-D-nIYyjL.js
+++ bundle/space3d-main-D-nIYyjL.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-DUj-P97L.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-CIikZ303.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
