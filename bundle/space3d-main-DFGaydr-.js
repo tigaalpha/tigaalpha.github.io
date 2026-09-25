471929a21f79d3382fb0195ec9f99b6d1acae5ff
TiGA Auto Release <actions@github.com>
2026-09-25 13:52:53 +0000

    Auto-release v13.7.450 (OTA)

--- bundle/space3d-main-DFGaydr-.js
+++ bundle/space3d-main-DFGaydr-.js
@@ -1,4 +1,4 @@
-import{r as Z,aq as cS,j as oe,ar as ep}from"./index.template-lMdX9evw.js";/**
+import{r as Z,aq as cS,j as oe,ar as ep}from"./index.template-D6n6Leba.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
