fdaa43d3dd0440f0ac84ce8a13b44e1ea34dec1b
TiGA Auto Release <actions@github.com>
2026-09-25 06:04:00 +0000

    Auto-release v13.7.424 (OTA)

--- bundle/space3d-main-DTAr03OS.js
+++ bundle/space3d-main-DTAr03OS.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-B7X6clkR.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-fvqtzowg.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
