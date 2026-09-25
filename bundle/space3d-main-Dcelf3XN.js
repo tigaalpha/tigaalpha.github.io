e8e119fe4692106a52479eb079c0a068938286a2
TiGA Auto Release <actions@github.com>
2026-09-25 01:36:39 +0000

    Auto-release v13.7.404 (OTA)

--- bundle/space3d-main-Dcelf3XN.js
+++ bundle/space3d-main-Dcelf3XN.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-zItsqx6E.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-DLhq5SBN.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
