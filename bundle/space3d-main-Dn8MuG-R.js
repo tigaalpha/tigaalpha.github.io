c84f00789421d0301613546f1c164586ce25bdd3
TiGA Auto Release <actions@github.com>
2026-09-25 01:42:47 +0000

    Auto-release v13.7.405 (OTA)

--- bundle/space3d-main-Dn8MuG-R.js
+++ bundle/space3d-main-Dn8MuG-R.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-Lmjn4Fc7.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-CWWu9N4N.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
