715a10655eccf2bc8d52db480c5f165604e5ba38
TiGA Auto Release <actions@github.com>
2026-09-25 13:12:21 +0000

    Auto-release v13.7.449 (OTA)

--- bundle/space3d-main-CKCDOdb6.js
+++ bundle/space3d-main-CKCDOdb6.js
@@ -1,4 +1,4 @@
-import{r as Z,aq as cS,j as oe,ar as ep}from"./index.template-CnaIU1qX.js";/**
+import{r as Z,aq as cS,j as oe,ar as ep}from"./index.template-DNZZtqwi.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
