3259faf30a69763f9ca0e546b60671740f4032f5
TiGA Auto Release <actions@github.com>
2026-09-25 07:10:47 +0000

    Auto-release v13.7.433 (OTA)

--- bundle/space3d-main-B-zanNYF.js
+++ bundle/space3d-main-B-zanNYF.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-DOVRNeOu.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-W2YmQKap.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
