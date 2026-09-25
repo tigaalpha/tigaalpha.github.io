e2760f110c954731c954d719b9e42dc4a3e149c3
TiGA Auto Release <actions@github.com>
2026-09-25 06:10:13 +0000

    Auto-release v13.7.426 (OTA)

--- bundle/space3d-main-kqc9IF_H.js
+++ bundle/space3d-main-kqc9IF_H.js
@@ -1,4 +1,4 @@
-import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-AZL_165c.js";/**
+import{r as Z,ao as cS,j as oe,ap as ep}from"./index.template-jIpfrY5-.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
