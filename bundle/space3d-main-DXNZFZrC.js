deb1b4e9888fa1852151ed6bfc24128f778a224f
TiGA Auto Release <actions@github.com>
2026-09-26 05:13:27 +0000

    Auto-release v13.7.452 (OTA)

--- bundle/space3d-main-DXNZFZrC.js
+++ bundle/space3d-main-DXNZFZrC.js
@@ -1,4 +1,4 @@
-import{r as Z,at as cS,j as oe,au as ep}from"./index.template-mKGSHV-d.js";/**
+import{r as Z,at as cS,j as oe,au as ep}from"./index.template-cGS61VAz.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
